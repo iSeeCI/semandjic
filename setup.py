185fe893521fc9564f11f7158307b99b9466b19b
jfernandrezj <andres.fernandez@iseeci.com>
2025-01-27 00:51:40 +0000

    Test on upsert bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="semandjic",
-    version="0.3.2",
+    version="0.3.3",
     author="Andres Fernandez",
     author_email="andres.fernandez@iseeci.com",
     description="A Django app for semantic model relationships and nested forms",
